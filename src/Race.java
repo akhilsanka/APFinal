3aee37d157727e6b8919a83f73f41880b655d6da
SanjanaKoka <happybird.koka@gmail.com>
2017-05-08 22:06:12 -0700

    added some method headers and fields

--- src/Race.java
+++ src/Race.java
@@ -1,4 +1,23 @@
 
 public class Race {
-
+	private User[] Users;
+	private Map map;
+	private String[] hints, answers;
+	
+	public Race()
+	{
+		
+	}
+	
+	private String[] shuffleHints()
+	{
+		String[] shuffle;
+		return shuffle;
+	}
+	
+	private String[] shuffleAnswers()
+	{
+		String[] shuffle;
+		return shuffle;
+	}
 }
