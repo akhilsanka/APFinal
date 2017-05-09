a4f61b4d11d2098147bd45b0595fb3512d53cca7
asanka565 <asanka565@LB28.hhs.win.fuhsd.org>
2017-05-09 13:19:15 -0700

    added stuff to race

--- src/Race.java
+++ src/Race.java
@@ -1,18 +1,28 @@
+import java.util.ArrayList;
 
 public class Race {
-	private User[] Users;
-	private Map map;
-	private String[] hints, answers;
+	private ArrayList<String> hints;
+	private ArrayList<Integer> answers;
+	private ArrayList<Integer> usedHints;
 	
 	public Race()
 	{
-		
+		hints = new ArrayList<String>();
+		answers = new ArrayList<Integer>();
 	}
 	
 	private String[] shuffleHints()
 	{
-		String[] shuffle;
-		return shuffle;
+		int randHint;
+		boolean used = false;
+		do{
+			randHint = (int)Math.random()*hints.size();
+		}
+		while(used != fale)
+			
+		}
+		for(int i = 0; i<hints.length(); i++)
+		
 	}
 	
 	private String[] shuffleAnswers()
