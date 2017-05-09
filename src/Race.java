75e8d3d5e52cf3fc1b6347130e188fedae1898d7
asanka565 <asanka565@LB28.hhs.win.fuhsd.org>
2017-05-09 14:09:15 -0700

    better version of getHint()

--- src/Race.java
+++ src/Race.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 
 public class Race {
 	
-	private ArrayList<Hint> hints;
-	private ArrayList<Integer> usedHints;
+	private ArrayList<Hint> unusedHints;
+	private ArrayList<Hint> usedHints;
 
 	private User[] Users;
 	private Map map;
@@ -12,36 +12,50 @@ public class Race {
 	
 	public Race()
 	{
-		hints = new ArrayList<Hint>();
+		unusedHints = new ArrayList<Hint>();
+		usedHints = new ArrayList<Hint>();
 		isComplete = false;
 	}
 	
+//	public Hint getHint()
+//	{
+//		if(usedHints.size() == hints.size()){
+//			return null;
+//		}
+//		int randHint;
+//		boolean used = false;
+//		do{
+//			randHint = (int)Math.random()*hints.size();
+//			for(int i = 0; i<usedHints.size(); i++){
+//				if(randHint == usedHints.get(i)){
+//					used = true;
+//					break;
+//				}
+//			}
+//			if(used == false){
+//				usedHints.add(randHint);
+//				return hints.get(randHint);
+//			}
+//			
+//		}
+//		while(used == true);
+//		return hints.get(0);
+//		
+//	}
 	public Hint getHint()
 	{
-		if(usedHints.size() == hints.size()){
+		if(unusedHints.size() == 0){
 			return null;
 		}
 		int randHint;
-		boolean used = false;
-		do{
-			randHint = (int)Math.random()*hints.size();
-			for(int i = 0; i<usedHints.size(); i++){
-				if(randHint == usedHints.get(i)){
-					used = true;
-					break;
-				}
-			}
-			if(used == false){
-				usedHints.add(randHint);
-				return hints.get(randHint);
-			}
-			
-		}
-		while(used == true);
-		return hints.get(0);
+		randHint = (int)Math.random()*unusedHints.size();
+		Hint shuffledHint = unusedHints.get(randHint);
+		usedHints.add(unusedHints.get(randHint));
+		unusedHints.remove(randHint);	
+		
+		return shuffledHint;
 		
 	}
-	
 	
 
 }
