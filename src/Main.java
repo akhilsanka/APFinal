94070e639ca1ea228dcb47bb701078abf8e47206
akhilsanka <akhilsanka@192.168.1.117>
2017-05-16 20:50:55 -0700

    some change to main?

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main extends JFrame {
 	//test 
 	JPanel cardPanel;
 	ArrayList<Race> races = new ArrayList<Race>();
-	private GamePanel panel5;
+	private PlayGamePanel panel5;
 	
 	public Main(String title) {
 		super(title);
@@ -26,7 +26,7 @@ public class Main extends JFrame {
 	    PlayGamePanel panel2 = new PlayGamePanel(this);
 	    MakeGamePanel panel3 = new MakeGamePanel(this);
 	    InstructionPanel panel4 = new InstructionPanel(this);
-	    panel5 = new GamePanel(this);
+	    panel5 = new PlayGamePanel(this);
 	    
 	    cardPanel.add(panel1,"1"); // Card is named "1"
 	    cardPanel.add(panel2,"2"); // Card is named "2"
@@ -87,6 +87,6 @@ public class Main extends JFrame {
 	
 	public void setRace(Race r)
 	{
-		panel5.setRace(r);
+		//panel5.setRace(r);
 	}
 }
